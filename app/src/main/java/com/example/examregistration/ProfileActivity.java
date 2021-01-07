1d53ab4e42dbffedf664322112316be335bbaae8
erickngumbau <57805450+erickngumbau@users.noreply.github.com>
2021-01-07 11:00:53 +0300

    profile edit

--- app/src/main/java/com/example/examregistration/ProfileActivity.java
+++ app/src/main/java/com/example/examregistration/ProfileActivity.java
@@ -2,19 +2,35 @@ package com.example.examregistration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private TextView name,registration,units;
+    private TextView name,registration,units,profileedit;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imageView = findViewById(R.id.image);
+        name = findViewById(R.id.username);
+        registration = findViewById(R.id.registration);
+        units=findViewById(R.id.units);
+        profileedit=findViewById(R.id.edit);
+
+        profileedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this,ProfileEditActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
