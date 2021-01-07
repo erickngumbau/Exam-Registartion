8efb093637f17885fa20ddb90fa7169a6abc1455
erickngumbau <57805450+erickngumbau@users.noreply.github.com>
2021-01-07 15:10:20 +0300

    More on UI

--- app/src/main/java/com/example/examregistration/ProfileActivity.java
+++ app/src/main/java/com/example/examregistration/ProfileActivity.java
@@ -5,13 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private ScrollView scrollView;
     private TextView name,registration,units,profileedit;
+    private RelativeLayout first;
+    private ListView listView;
 
 
     @Override
@@ -24,12 +34,32 @@ public class ProfileActivity extends AppCompatActivity {
         registration = findViewById(R.id.registration);
         units=findViewById(R.id.units);
         profileedit=findViewById(R.id.edit);
+        first = findViewById(R.id.firstlayout);
+        listView =findViewById(R.id.listview);
+
+
+        ArrayList<String> units = new ArrayList<>();
+        units.add("BCS 110 Programming in Java");
+        units.add("BCS 111 Programming in Python");
+        units.add("BCS 112 Programming in C++");
+        units.add("BCS 113 Programming in C");
+        units.add("BCS 114 Programming in Spring");
+        units.add("BCS 114 Programming in PHP");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+            this,
+                android.R.layout.simple_list_item_1,
+                units
+        );
+
+        listView.setAdapter(adapter);
+
 
         profileedit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ProfileActivity.this,ProfileEditActivity.class);
-                startActivity(intent);
+              Intent intent = new Intent(ProfileActivity.this,SignUpActivity.class);
+              startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/examregistration/SignUpActivity.java
+++ app/src/main/java/com/example/examregistration/SignUpActivity.java
@@ -3,12 +3,34 @@ package com.example.examregistration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText name,date, reg,password;
+    private TextView upload,unitselect;
+    private ImageView imageView;
+    private RelativeLayout layout1;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        name=findViewById(R.id.name);
+        date=findViewById(R.id.date);
+        reg=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        unitselect=findViewById(R.id.unitselect);
+        upload=findViewById(R.id.upload);
+        imageView=findViewById(R.id.image);
+
+        spinner=findViewById(R.id.spinner);
+
     }
 }
